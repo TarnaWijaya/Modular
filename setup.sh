79fc4b095bb1ae4f221eee0a7b9779a625c878d5
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-28 04:00:02 +0700

    update setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+echo"
+   _____             .___    .__                
+  /     \   ____   __| _/_ __|  | _____ _______ 
+ /  \ /  \ /  _ \ / __ |  |  \  | \__  \\_  __ \
+/    Y    (  <_> ) /_/ |  |  /  |__/ __ \|  | \/
+\____|__  /\____/\____ |____/|____(____  /__|   
+        \/            \/               \/       "
+sleep 1
+echo" {Start up Modular by TarnaWijaya} "
+echo"Version: 1.0
+     Developer: TarnaWijaya
+     Name Hp: $(getprop ro.product.system.brand)
+     Negara: $(getprop ro.product.country.region)"
+sleep 2
+echo"--{ Welcome To Modular }--"
+echo"XTarzAI Product"
+echo"_______________________________________________"
+echo"Memuat..."
+  # perintah
+echo"Berhasil Terpsang.."
+
+
+
+
+# end
